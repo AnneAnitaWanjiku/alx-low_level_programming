0bbd9651549af3631c023eec9e682f99dacda547
AnneAnitaWanjiku <wanjikuanita7243@gmail.com>
2022-12-15 02:13:04 -0800

    absolutr value

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
  * main - Entry point
  * _islower - Checks for lowercase character
  * @c: The character to be checked
- * Return 1 for lowercase or 0 for anything else
+ * Return: 1 for lowercase or 0 for anything else
  */
 int _islower(int c)
 {
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The value to find
+ * Return: the absolute value of an integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
+
