ad1f82875ae5803cf7ee7b6133986f74a2e243d6
AnneAnitaWanjiku <wanjikuanita7243@gmail.com>
2022-12-19 06:47:55 -0800

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,3 +2,18 @@
 
 /**
  * print_square - print hashes square
+ * @size: size of the square
+ *
+ * Return: no return
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; size++)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+}
+
