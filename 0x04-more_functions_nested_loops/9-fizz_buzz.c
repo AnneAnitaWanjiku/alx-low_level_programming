35fb2f37fc33048183a1d8e6708d6954061b1393
AnneAnitaWanjiku <wanjikuanita7243@gmail.com>
2022-12-19 07:41:39 -0800

    https://alx-intranet.hbtn.io/projects/215#quiz-completed:~:text=9.-,Fizz%2DBuzz,-mandatory

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100, followed by a new lin
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+n = 1
+printf("%d", n);
+for (n = 2; n <= 100; n++)
+{
+if ((n % 3 == 0) && (n % 5 != 0))
+{
+printf(" Fizz");
+}
+else if (n % 5 == 0)
+printf(" Buzz");
+}
+else if (n % 3 == 0)
+{
+printf(" FizzBuzz");
+}
+{
+else
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
