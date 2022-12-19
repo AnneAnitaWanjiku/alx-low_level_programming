3be6afede4808065c3a0fa93a9281cb21358a71e
AnneAnitaWanjiku <wanjikuanita7243@gmail.com>
2022-12-19 03:59:35 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line n times
+ * @n: times line is printed
+ * Return: no return
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; n++)
+	{
+		_putchar(95);
+	}
+}
