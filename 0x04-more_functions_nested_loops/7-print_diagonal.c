25a6ff46b2aaa3e98bc1a979d87417c21d7aa911
AnneAnitaWanjiku <wanjikuanita7243@gmail.com>
2022-12-19 23:36:28 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,22 +6,21 @@
  */
 void print_diagonal(int n)
 {
+int i, j;
+
+
 if (n <= 0)
 {
  _putchar('\n');
 }
-else
 {
-int i, j;
-
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < i; j++)
 {
-if (j == i)
-else if (j < i)
-_putchar('');
+_putchar(' ');
 }
+_putchar(92);
 _putchar('\n');
 }
 }
