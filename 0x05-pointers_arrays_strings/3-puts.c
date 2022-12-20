3064e4d616dc0e83d4fbd83033c431867ae95854
AnneAnitaWanjiku <wanjikuanita7243@gmail.com>
2022-12-20 03:52:09 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_puts - puts a string
+ *@str: the string to print
+ *Return: void
+ */
+void _puts(char *str)
+{
+	for (; *str != '0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
