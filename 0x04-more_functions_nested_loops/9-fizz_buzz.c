17fbd87a1863d367e6a8e2bd71a28bd6ff8dc4e9
AnneAnitaWanjiku <wanjikuanita7243@gmail.com>
2022-12-20 00:59:30 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the numbers from 1 to 100, followed by a new lin
@@ -10,24 +11,24 @@ int main(void)
 {
 int n;
 
-n = 1
-printf("%d", n);
-for (n = 2; n <= 100; n++)
+for (n = 1; n <= 100; n++)
 {
-if ((n % 3 == 0) && (n % 5 != 0))
+if ((n % 3 == 0) && (n % 5 == 0))
+{
+printf(" FizzBuzz");
+}
+else if (n % 3 == 0)
 {
 printf(" Fizz");
 }
 else if (n % 5 == 0)
-printf(" Buzz");
-}
-else if (n % 3 == 0)
 {
-printf(" FizzBuzz");
+printf("Buzz");
 }
+if (n != 100)
 {
-else
-printf("%d", n);
+print(" ");
+}
 }
 printf("\n");
 return (0);
